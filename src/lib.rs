c8130b4bc8dfac0361bc618e5d833ced831e48f3
Blair Edwards <blaired1@yahoo.co.uk>
2024-09-14 02:06:20 +0100

    Copied over a Rust template. Copied the logo-widget code from my '24-loading-logos' codebase, including 'loading_logos.rs'. Wrote an unsafe C API function `sp_gtk4_loading_logos_create ()`, which immediately calls the safe `priv_create ()` function.  This seems to work great. Provided the DrawingArea with a minimum size - I'll probably want to implement some kind of 'expand-on-create' for this.  This had me stumped for a while and I couldn't work out why the widget wasn't visible - this was the cause. Added a box around the widget for clarity - this will go away in time.

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,162 @@
-//#  I should have the unsafe functions call safe functions.  Maybe even abstract them to a different module?  Or file?
+/*  *--<Preface>--*  //
 
+ -=-  Author Details  -=-
+ Blair Edwards
+ My own shenanigans.
+
+ -=-  Dates  -=-
+ Started 2024-09-09
+
+ -=-  Description  -=-
+ This library provides an API for creating logo-style loading spinners.
+ IO is through API calls via a C-style header and archive.
+ Config is through compile-time 'const's and API calls.
+
+ -=-  Task  -=-
+ -=>
+
+ -=-  Notes  -=-
+ -=>  I've developed my own commenting notation for things that "aren't done" one way or another.  Such as:
+	 -  //#  TODO
+	 -  //?  Not sure / query
+	 -  //!  Important note / relevant as technology advances
+ -=>  Logging with `_LOG ()` takes a 'logLevel' argument, which roughly indicates:
+	 -  0:  Critical, major errors, should Always be printed.
+	 -  1:	Important info particularly critical functions, minor / user errors.
+	 -  2:	Useful info / general programme flow.
+	 -  3:	Debug info, steps throughout a function.
+	 -  4:  Useful spam - printed often such as in a loop.
+	 -  5:  Debug spam - printed often such as in a loop.
+ -=>
+
+//  *--</Preface>--*  */
+
+
+
+//  *--<Preparations>--*  //
+
+//  Local Compiler Pragmas
+#! [allow (unused_variables)]
+#! [allow (non_snake_case)]
+#! [allow (unused_parens)]
+#! [allow (dead_code)]
+#! [allow (unused_imports)]
+
+//  Imports
+//use std ::io ::{stdin, stdout};
+//use std ::io ::Result;
+//use std ::time;
+//use std ::fs ::File;
+//use std ::path ::Path;
+//use std ::io ::{Read, BufReader, BufRead};
+//use std ::io ::{Write, BufWriter};
+use std ::f64 ::consts ::PI;
+use std ::sync ::atomic ::{AtomicUsize, Ordering};
+//use std ::rc ::Rc;
+//use std ::cell ::RefCell;
 //  of Which are GTK4
 use gtk4 as gtk;
 use gtk ::prelude ::*;
 use gtk ::glib;
 use gtk ::glib ::clone;
+//  of Which are Local
+mod loading_logos;
+use loading_logos ::LoadingLogo;
+
+//  Global Enumerations
+
+//  Global Constants
+const TIME_ANIM: u16 = 50;
+const DRAW_TARGET_LEN: f64 = 1000.0;
+const DRAW_LINE_WIDTH_BASE: f64 = 10.0;
+
+//  Global Variables
+
+//  Local Constants
+
+//  Local Variables
+
+//  Structures
+
+//  *--</Preparations>--*  //
+
+
+
+//  *--<Macros>--*  //
+
+//  *--</Macros>--*  //
+
+
+
+//  *--<Traits & Implementations>--*  //
+
+//  *--</Traits & Implementations>--*  //
+
+
+
+//  *--<Main Code>--*  //
 
 # [no_mangle]
-pub unsafe extern "C" fn sp_gtk4_loading_logos_create () -> gtk ::Label
+pub unsafe extern "C" fn sp_gtk4_loading_logos_create () -> gtk ::Widget
 {
-    let label_logo = gtk ::Label ::builder ()
-		//.text ("Hello from the library!")
+	return priv_create ();
+}
+
+fn priv_create () -> gtk ::Widget
+{
+	//  Attach the Cairo canvas.
+	let cairo_loading = gtk ::DrawingArea ::builder ()
+		.content_width (400)
+		.content_height (400)
 		.build ();
-	label_logo .set_text ("Hello from the library!");
-	return label_logo;
+	cairo_loading .set_draw_func (cairo_loading_render);
+	//  Add a timeout to update the logo's positions.
+	//  Local, so we don't mess with GTK's main-thread requirements.
+	gtk ::glib ::timeout_add_local
+	(
+		core ::time ::Duration ::from_millis (TIME_ANIM as u64),
+		clone! (@strong cairo_loading => move ||
+		{
+			cairo_loading .queue_draw ();
+			return true .into ();
+		}
+	));
+	return cairo_loading .into ();
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+//  *--</Main Code>--*  //
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+
+
+//  *--<Callbacks>--*  //
+
+fn cairo_loading_render (area: &gtk ::DrawingArea, cairo: &gtk ::cairo ::Context, width: i32, height: i32)
+{
+	//  'static' iteration counter.
+	//#  Handle overflow.
+	static ITER: AtomicUsize = AtomicUsize ::new (0);
+	let iter = ITER .fetch_add (1, Ordering ::Relaxed) as f64;
+
+	//  Draw a box outline to help suss out the widget's area.
+	cairo .rectangle (0.0, 0.0, width as f64, height as f64);
+
+	//  Scale factor - calculated from 'width' and 'height'.
+	let areaScale = core ::cmp ::min (width, height) as f64 / DRAW_TARGET_LEN;
+
+	//  Move the origin to the middle and flip the Y-axis.
+	let matrix = gtk ::cairo ::Matrix ::new (1.0, 0.0, 0.0, -1.0, width as f64 / 2.0, height as f64 / 2.0);
+	cairo .transform (matrix);
+
+	//  Perform the draw.
+	let ANIM_TYPE: LoadingLogo = LoadingLogo ::OrbitNBalls;
+	ANIM_TYPE .draw (cairo, iter, areaScale);
+
+	//  Render that line.
+	cairo .set_line_width (DRAW_LINE_WIDTH_BASE * areaScale);
+	cairo .set_line_cap (gtk ::cairo ::LineCap ::Round);
+	cairo .set_line_join (gtk ::cairo ::LineJoin ::Round);
+	cairo .set_source_rgba (1.0, 1.0, 1.0, 1.0);
+	cairo .stroke () .unwrap ();
 }
+
+//  *--</Callbacks>--*  //
---
+++ src/loading_logos.rs
@@ -0,0 +1,415 @@
+/*  *--<Preface>--*  //
+
+ -=-  Author Details  -=-
+ Blair Edwards
+ My own shenanigans.
+
+ -=-  Part Of  -=-
+ sp-gtk4-loading-logos.git
+
+//  *--</Preface>--*  */
+
+
+
+//  *--<Doc>--*  */
+
+//!  Documentation!
+
+//  *--</Doc>--*  */
+
+
+
+//  *--<Preparations>--*  //
+
+//  Local Compiler Pragmas
+#! [allow (unused_variables)]
+#! [allow (non_snake_case)]
+#! [allow (unused_parens)]
+#! [allow (dead_code)]
+#! [allow (unused_imports)]
+
+//  Imports
+//use std ::io ::{stdin, stdout};
+//use std ::io ::Result;
+//use std ::time;
+//use std ::fs ::File;
+//use std ::path ::Path;
+//use std ::io ::{Read, BufReader, BufRead};
+//use std ::io ::{Write, BufWriter};
+use std ::f64 ::consts ::PI;
+//  of Which are GTK4
+use gtk4 as gtk;
+//use gtk ::prelude ::*;
+//use gtk ::glib;
+//use gtk ::glib ::clone;
+//  of Which are Local
+
+//  Global Enumerations
+
+/*  Animation Types
+1.  Pulsing radius-filling squircle.
+2.  Circumference-filling circle CCW.
+3.  Circumference-filling circle, CW.
+4.  Orbiting N-Ary balls.
+5.  Circumference-filling circle, with N-Ary orbiting balls.
+6.  N-start circumference-following arcs.
+7.  Concentric reverse-direction circumference-following circles.
+8.  Concentric reverse-direction circumference-following circles V2.
+9.  Concentric reverse-direction circumference-following circles, multi-speed.
+10.  Orbiting N-Ary balls, with radius lines.
+11.  Orbiting N-Ary balls, with radius-following pulsers.
+*/
+# [allow (non_camel_case_types)]
+pub enum LoadingLogo
+{
+	PulseFillCircle,
+	CircFillCircleCcw,
+	CircFillCircleCw,
+	OrbitNBalls,
+	CircFillCircle_OrbitNBalls,
+	NStartCircArcs,
+	ConcentricCircArcsV1,
+	ConcentricCircArcsV2,
+	ConcentricCircArcsV3,
+	OrbitNBalls_RadLines,
+	OrbitNBalls_PulseRadLines,
+	Pong
+}
+
+//  Global Constants
+
+//  Global Variables
+
+//  Local Constants
+
+//  Local Variables
+
+//  *--</Preparations>--*  //
+
+
+
+//  *--<Errors>--*  */
+
+/*pub enum Error
+{
+	Nope,
+}
+impl std ::fmt ::Display for Error
+{
+	fn fmt (&self, fmtr: &mut std ::fmt ::Formatter) -> std ::fmt ::Result
+	{
+		let errMsg = match self
+		{
+			Error ::Nope => "Nope error reason.",
+		};
+		return write! (fmtr, "{}", errMsg);
+	}
+}
+impl std ::fmt ::Debug for Error
+{
+	fn fmt (&self, fmtr: &mut std ::fmt ::Formatter) -> std ::fmt ::Result
+	{
+		let errMsg = match self
+		{
+			Error ::Nope => "Nope error debug reason.",
+		};
+		return write! (fmtr, "{}", errMsg);
+	}
+}*/
+
+//  *--</Errors>--*  */
+
+
+
+//  *--<Macros>--*  //
+
+/*macro_rules! macroName
+{
+	($a :expr, $b :expr) =>
+	{
+		$a + $b
+	}
+	($a :expr) =>
+	{
+		$a
+	}
+}*/
+
+//  *--</Macros>--*  //
+
+
+
+//  *--<Traits & Implementations>--*  //
+
+impl LoadingLogo
+{
+	//Work out how to break these down into smaller fucntions or something.  Does Rust have private impl functions?
+	pub fn draw (self, cairo: &gtk ::cairo ::Context, iter: f64, areaScale: f64)
+	{
+		match (self)
+		{
+			//#  Parameterise more of these.
+			LoadingLogo ::PulseFillCircle => self .draw_PulseFillCircle (cairo, iter, areaScale),
+			LoadingLogo ::CircFillCircleCcw => self .draw_CircFillCircleCcw (cairo, iter, areaScale),
+			LoadingLogo ::CircFillCircleCw => self .draw_CircFillCircleCw (cairo, iter, areaScale),
+			LoadingLogo ::OrbitNBalls => self .draw_OrbitNBalls (cairo, iter, areaScale),
+			LoadingLogo ::CircFillCircle_OrbitNBalls => self .draw_CircFillCircle_OrbitNBalls (cairo, iter, areaScale),
+			LoadingLogo ::NStartCircArcs => self .draw_NStartCircArcs (cairo, iter, areaScale),
+			LoadingLogo ::ConcentricCircArcsV1 => self .draw_ConcentricCircArcsV1 (cairo, iter, areaScale),
+			LoadingLogo ::ConcentricCircArcsV2 => self .draw_ConcentricCircArcsV2 (cairo, iter, areaScale),
+			LoadingLogo ::ConcentricCircArcsV3 => self .draw_ConcentricCircArcsV3 (cairo, iter, areaScale),
+			LoadingLogo ::OrbitNBalls_RadLines => self .draw_OrbitNBalls_RadLines (cairo, iter, areaScale),
+			LoadingLogo ::OrbitNBalls_PulseRadLines => self .draw_OrbitNBalls_PulseRadLines (cairo, iter, areaScale),
+			LoadingLogo ::Pong => todo! (),
+		}
+	}
+
+	fn draw_PulseFillCircle (self, cairo: &gtk ::cairo ::Context, iter: f64, areaScale: f64)
+	{
+		let iterScaled = iter * 3.0;
+		let radMax = 100.0 * areaScale;
+		if (iterScaled % (radMax * 2.0) <= radMax)
+		{
+			cairo .move_to (iterScaled % (radMax * 2.0), 0.0);
+			cairo .arc (0.0, 0.0, iterScaled % (radMax * 2.0), 0.0, PI * 2.0);
+		}
+		else
+		{
+			cairo .move_to (radMax * 2.0 - iterScaled % (radMax * 2.0), 0.0);
+			cairo .arc (0.0, 0.0, radMax * 2.0 - iterScaled % (radMax * 2.0), 0.0, PI * 2.0);
+		}
+	}
+	fn draw_CircFillCircleCcw (self, cairo: &gtk ::cairo ::Context, iter: f64, areaScale: f64)
+	{
+		let iterScaled = iter * 0.26 % (PI * 4.0);
+		let radMax = 100.0;
+		cairo .move_to (radMax, 0.0);
+		match (iterScaled <= PI * 2.0)
+		{
+			true => cairo .arc (0.0, 0.0, radMax, 0.0, iterScaled),
+			false => cairo .arc_negative (0.0, 0.0, radMax, 0.0, iterScaled),
+		};
+	}
+	fn draw_CircFillCircleCw (self, cairo: &gtk ::cairo ::Context, iter: f64, areaScale: f64)
+	{
+		let iterScaled = iter * 0.26 % (PI * 4.0);
+		let iterRev = PI * 2.0 - iterScaled;
+		let radMax = 100.0;
+		cairo .move_to (radMax * iterRev .cos (), radMax * iterRev .sin ());
+		match (iterScaled <= PI * 2.0)
+		{
+			true => cairo .arc (0.0, 0.0, radMax, iterRev, PI * 2.0),
+			false => cairo .arc_negative (0.0, 0.0, radMax, iterRev, PI * 2.0),
+		};
+	}
+	fn draw_OrbitNBalls (self, cairo: &gtk ::cairo ::Context, iter: f64, areaScale: f64)
+	{
+		let radCircle = 20.0 * areaScale;
+		let radOrbit = 100.0 * areaScale;
+		let countCircle = 3;
+		let iterScaled = iter * 0.1;
+
+		for circle in 0..countCircle
+		{
+			let iterStart = (iter * 0.1 + PI * 2.0 * circle as f64 / countCircle as f64) % (PI * 2.0);
+			cairo .move_to (radCircle + radOrbit * iterStart .cos (), radOrbit * iterStart .sin ());
+			cairo .arc (radOrbit * iterStart .cos (), radOrbit * iterStart .sin (), radCircle, 0.0, PI * 2.0);
+		}
+	}
+	fn draw_CircFillCircle_OrbitNBalls (self, cairo: &gtk ::cairo ::Context, iter: f64, areaScale: f64)
+	{
+		let radOuter = 200.0;
+		let radOrbit = 100.0;
+		let radCircle = 20.0;
+		let countCircle = 3;
+		let iterCirc = (iter * 0.26) % (PI * 4.0);
+		cairo .move_to (radOuter, 0.0);
+		match (iterCirc <= PI * 2.0)
+		{
+			true => cairo .arc (0.0, 0.0, radOuter, 0.0, iterCirc),
+			false => cairo .arc_negative (0.0, 0.0, radOuter, 0.0, iterCirc),
+		};
+
+		for circle in 0..countCircle
+		{
+			let iterStart = (iter * 0.1 + PI * 2.0 * circle as f64 / countCircle as f64) % (PI * 2.0);
+			cairo .move_to (radCircle + radOrbit * iterStart .cos (), radOrbit * iterStart .sin ());
+			cairo .arc (radOrbit * iterStart .cos (), radOrbit * iterStart .sin (), radCircle, 0.0, PI * 2.0);
+		}
+	}
+	fn draw_NStartCircArcs (self, cairo: &gtk ::cairo ::Context, iter: f64, areaScale: f64)
+	{
+		let starts = 5;
+		let iterCirc = (iter * 0.07) % (PI * 2.0);
+		let radCircle = 120.0;
+		let lengthArc = 0.4;
+		for start in 0..starts
+		{
+			let iterStart = iterCirc + PI * 2.0 * start as f64 / starts as f64;
+			cairo .move_to (radCircle * iterStart .cos (), radCircle * iterStart .sin ());
+			cairo .arc (0.0, 0.0, radCircle, iterStart, iterStart + lengthArc);
+		}
+	}
+	fn draw_ConcentricCircArcsV1 (self, cairo: &gtk ::cairo ::Context, iter: f64, areaScale: f64)
+	{
+		let iterScale = (iter * 0.26) % (PI * 4.0);
+		let radStart = 80.0;
+		let radSpace = 25.0;
+		let countCircle = 3;
+		for circle in 0..countCircle
+		{
+			let rad = radStart + (radSpace * circle as f64);
+			if (circle % 2 == 0)
+			{
+				cairo .move_to (rad, 0.0);
+				match (iterScale <= PI * 2.0)
+				{
+					true => cairo .arc (0.0, 0.0, rad, 0.0, iterScale),
+					false => cairo .arc_negative (0.0, 0.0, rad, 0.0, iterScale),
+				};
+			}
+			else
+			{
+				let iterRev = PI * 4.0 - iterScale;
+				cairo .move_to (rad * iterRev .cos (), rad * iterRev .sin ());
+				match (iterScale <= PI * 2.0)
+				{
+					true => cairo .arc (0.0, 0.0, rad, iterRev, PI * 2.0),
+					false => cairo .arc_negative (0.0, 0.0, rad, iterRev, PI * 2.0),
+				};
+			}
+		}
+	}
+	fn draw_ConcentricCircArcsV2 (self, cairo: &gtk ::cairo ::Context, iter: f64, areaScale: f64)
+	{
+		let iterScale = (iter * 0.26) % (PI * 4.0);
+		let radStart = 80.0;
+		let radSpace = 25.0;
+		let countCircle = 3;
+		for circle in 0..countCircle
+		{
+			let rad = radStart + (radSpace * circle as f64);
+			if (circle % 2 == 0)
+			{
+				cairo .move_to (rad, 0.0);
+				match (iterScale <= PI * 2.0)
+				{
+					true => cairo .arc (0.0, 0.0, rad, 0.0, iterScale),
+					false => cairo .arc_negative (0.0, 0.0, rad, 0.0, iterScale),
+				};
+			}
+			else
+			{
+				let iterRev = PI * 4.0 - iterScale;
+				cairo .move_to (rad * (iterRev + PI) .cos (), rad * (iterRev + PI) .sin ());
+				match (iterScale <= PI * 2.0)
+				{
+					true => cairo .arc (0.0, 0.0, rad, iterRev + PI, PI),
+					false => cairo .arc_negative (0.0, 0.0, rad, iterRev + PI, PI),
+				};
+			}
+		}
+	}
+	fn draw_ConcentricCircArcsV3 (self, cairo: &gtk ::cairo ::Context, iter: f64, areaScale: f64)
+	{
+		let iterScale = iter * 0.1;
+		let radStart = 80.0;
+		let radSpace = 25.0;
+		let countCircle = 3;
+		for circle in 0..countCircle
+		{
+			let rad = radStart + (radSpace * circle as f64);
+			let iterScaleSpd = (iterScale + iterScale * 0.5 * circle as f64) % (PI * 4.0);
+			if (circle % 2 == 0)
+			{
+				cairo .move_to (rad, 0.0);
+				match (iterScaleSpd <= PI * 2.0)
+				{
+					true => cairo .arc (0.0, 0.0, rad, 0.0, iterScaleSpd),
+					false => cairo .arc_negative (0.0, 0.0, rad, 0.0, iterScaleSpd),
+				};
+			}
+			else
+			{
+				let iterScaleRev = PI * 4.0 - iterScaleSpd;
+				cairo .move_to (rad * iterScaleRev .cos (), rad * iterScaleRev .sin ());
+				match (iterScaleRev <= PI * 2.0)
+				{
+					true => cairo .arc_negative (0.0, 0.0, rad, iterScaleRev, PI * 2.0),
+					false => cairo .arc (0.0, 0.0, rad, iterScaleRev, PI * 2.0),
+				};
+			}
+		}
+	}
+	fn draw_OrbitNBalls_RadLines (self, cairo: &gtk ::cairo ::Context, iter: f64, areaScale: f64)
+	{
+		let radCircle = 20.0;
+		let radOrbit = 200.0;
+		let sparkStart = 30.0;
+		let sparkGap = 40.0;
+		let countCircle = 3;
+
+		for circle in 0..countCircle
+		{
+			let iterStart = (iter * 0.1 + PI * 2.0 * circle as f64 / countCircle as f64) % (PI * 2.0);
+			//  Spark Line
+			cairo .move_to (sparkStart * iterStart .cos (), sparkStart * iterStart .sin ());
+			cairo .line_to ((radOrbit - radCircle / 2.0 - sparkGap) * iterStart .cos (), (radOrbit - radCircle / 2.0 - sparkGap) * iterStart .sin ());
+			//  Circle
+			cairo .move_to (radCircle + radOrbit * iterStart .cos (), radOrbit * iterStart .sin ());
+			cairo .arc (radOrbit * iterStart .cos (), radOrbit * iterStart .sin (), radCircle, 0.0, PI * 2.0);
+		}
+	}
+	fn draw_OrbitNBalls_PulseRadLines (self, cairo: &gtk ::cairo ::Context, iter: f64, areaScale: f64)
+	{
+		let radCircle = 20.0;
+		let radOrbit = 200.0;
+		let sparkStart = 30.0;
+		let sparkGap = 40.0;
+		let sparkStop = (radOrbit - radCircle / 2.0 - sparkGap);
+		let countCircle = 3;
+		let iterSpark = (iter * 8.0) % ((sparkStop - sparkStart) * 2.0);
+
+		for circle in 0..countCircle
+		{
+			let iterStart = (iter * 0.1 + PI * 2.0 * circle as f64 / countCircle as f64) % (PI * 2.0);
+			//  Spark Line
+			match (iterSpark <= sparkStop - sparkStart)
+			{
+				true =>
+				{
+					cairo .move_to (sparkStart * iterStart .cos (), sparkStart * iterStart .sin ());
+					cairo .line_to ((sparkStart + iterSpark) * iterStart .cos (), (sparkStart + iterSpark) * iterStart .sin ());
+				},
+				false =>
+				{
+					let iterSpark = iterSpark % (sparkStop - sparkStart);
+					cairo .move_to ((sparkStart + iterSpark) * iterStart .cos (), (sparkStart + iterSpark) * iterStart .sin ());
+					cairo .line_to (sparkStop * iterStart .cos (), sparkStop * iterStart .sin ());
+				},
+			};
+			//  Circle
+			cairo .move_to (radCircle + radOrbit * iterStart .cos (), radOrbit * iterStart .sin ());
+			cairo .arc (radOrbit * iterStart .cos (), radOrbit * iterStart .sin (), radCircle, 0.0, PI * 2.0);
+		}
+	}
+}
+
+//  *--</Traits & Implementations>--*  //
+
+
+
+//  *--<Main Code>--*  //
+
+/*fn subLocal () -> u8
+{
+	return 0;
+}*/
+
+//  *--</Main Code>--*  //
+
+
+
+//  *--<Callbacks>--*  //
+
+//  *--</Callbacks>--*  //
